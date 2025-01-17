e8006bae4e0bffab7316c1daeb97ecdb6473fed8
jamesmedina7787 <jameswilfredmedina@gmail.com>
2025-01-17 00:43:40 -0500

    finally found out that a zombie port was clogging stuff up with sudo systemctl restart docker, that was a pain

--- backend/server.js
+++ backend/server.js
@@ -49,5 +49,5 @@ app.use('/workshops', workshopRoutes);
 app.use('/classes', classRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORTT || 3001;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
